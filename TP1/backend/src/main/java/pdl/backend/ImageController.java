15b5f377508a9beb70de683e53c98afbe9a66bb6
Yli <75490693+qpYliqp@users.noreply.github.com>
2023-01-16 13:35:06 +0100

    Initial commit

---
+++ TP1/backend/src/main/java/pdl/backend/ImageController.java
@@ -0,0 +1,119 @@
+ package pdl.backend;
+
+ import java.io.IOException;
+
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.http.HttpStatus;
+ import org.springframework.http.MediaType;
+ import org.springframework.http.ResponseEntity;
+ import org.springframework.web.bind.annotation.PathVariable;
+ import org.springframework.web.bind.annotation.RequestMapping;
+ import org.springframework.web.bind.annotation.RequestMethod;
+ import org.springframework.web.bind.annotation.RequestParam;
+ import org.springframework.web.bind.annotation.ResponseBody;
+ import org.springframework.web.bind.annotation.RestController;
+ import org.springframework.web.multipart.MultipartFile;
+ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+ import com.fasterxml.jackson.databind.ObjectMapper;
+ import com.fasterxml.jackson.databind.node.ArrayNode;
+ import com.fasterxml.jackson.databind.node.ObjectNode;
+
+ @RestController
+ public class ImageController {
+
+   @Autowired
+   private ObjectMapper mapper;
+
+   private final ImageDao imageDao;
+
+   @Autowired
+   public ImageController(ImageDao imageDao) {
+     this.imageDao = imageDao;
+   }
+
+   @RequestMapping(value = "/images/{id}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+   public ResponseEntity<?> getImage(@PathVariable("id") long id) {
+
+     try{
+
+      var imgFile = imageDao.retrieve(id);
+      //System.out.println("Do I have the image ?"+imgFile.get().getName());
+ 
+       byte[] bytes = imgFile.get().getData();
+
+      return ResponseEntity
+              .ok()
+              .contentType(MediaType.IMAGE_JPEG)
+              .body(bytes);
+     }
+     catch(Exception e){
+       System.out.println("I dont have the image");
+         e.printStackTrace();
+         return ResponseEntity
+         .status(HttpStatus.NOT_FOUND)
+         .body("Image not found");
+     }
+     
+     
+   }
+
+   @RequestMapping(value = "/images/{id}", method = RequestMethod.DELETE)
+   public ResponseEntity<?> deleteImage(@PathVariable("id") long id) {
+     var imgFile = imageDao.retrieve(id);
+     if(imgFile.isPresent())
+     {
+       this.imageDao.delete(imgFile.get());
+     return ResponseEntity
+             .ok()
+             .contentType(MediaType.IMAGE_JPEG)
+             .body("Image deleted");
+     }
+     else{
+       return ResponseEntity
+             .ok()
+             .contentType(MediaType.IMAGE_JPEG)
+             .body("Image doesn't exist");
+     }
+    
+   }
+
+   @RequestMapping(value = "/images", method = RequestMethod.POST)
+   public ResponseEntity<?> addImage(@RequestParam("file") MultipartFile file,
+       RedirectAttributes redirectAttributes) throws IOException {
+        
+         if(file.getContentType().equals("image/jpeg") || file.getContentType().equals("image/jpg"))
+         {
+           Image img = new Image(file.getOriginalFilename(), file.getBytes());
+           this.imageDao.create(img);
+           return ResponseEntity
+         .ok()
+         .contentType(MediaType.IMAGE_JPEG)
+         .body("Image added\n");
+         }
+         else
+         {
+           return ResponseEntity
+           .status(HttpStatus.NOT_FOUND)
+           .contentType(MediaType.IMAGE_JPEG)
+           .body("Image not found\n");
+         }
+     
+        
+   }
+
+   @RequestMapping(value = "/images", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+   @ResponseBody
+   public ArrayNode getImageList() {
+     ArrayNode nodes = mapper.createArrayNode();
+     for(Image img : imageDao.retrieveAll())
+     {
+       ObjectNode node = mapper.createObjectNode();
+       node.put("id", img.getId());
+       node.put("name", img.getName());
+       nodes.add(node);
+     }
+     return nodes;
+   }
+
+ }
---
+++ TP1/backend/src/main/java/pdl/backend/ImageDao.java
@@ -0,0 +1,62 @@
+package pdl.backend;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ImageDao implements Dao<Image> {
+
+  private final Map<Long, Image> images = new HashMap<>();
+
+  public ImageDao() {
+    // placez une image test.jpg dans le dossier "src/main/resources" du projet
+    final ClassPathResource imgFile = new ClassPathResource("test.jpg");
+    byte[] fileContent;
+    try {
+      fileContent = Files.readAllBytes(imgFile.getFile().toPath());
+      Image img = new Image("logo.jpg", fileContent);
+      images.put(img.getId(), img);
+    } catch (final IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public Optional<Image> retrieve(final long id) {
+    return Optional.of(this.images.get(id));
+  }
+
+  @Override
+  public List<Image> retrieveAll() {
+    ArrayList<Image> list = new ArrayList<Image>();
+    for(int i = 0 ; i < images.size() ; i++)
+    {
+      list.add(images.get((long) i));
+    }
+      
+    return list;
+  }
+
+  @Override
+  public void create(final Image img) {
+    this.images.put(img.getId(),img);
+  }
+
+  @Override
+  public void update(final Image img, final String[] params) {
+    // Not used
+  }
+
+  @Override
+  public void delete(final Image img) {
+    this.images.remove(img.getId());
+  }
+}
