66cdba06b53296f9383356639dcc35991e1d4807
Yli <75490693+qpYliqp@users.noreply.github.com>
2023-01-19 00:36:46 +0100

    Starting FrontEnd

--- TP1/frontend/src/components/HelloWorld.vue
+++ TP1/frontend/src/components/HelloWorld.vue
@@ -1,16 +1,61 @@
 <script setup lang="ts">
+
 import { ref } from 'vue'
+import { defineProps } from 'vue'
+import axios from 'axios'
+import { json } from 'stream/consumers';
+
+//declare a variable null in vue.js
+var imgs = "";
+ 
+ function test() { axios.get('http://localhost:4000/images')
+  .then(response => {
+    console.log(response.data)
+    
+    var imgs = JSON.stringify(response.data[0].name);
+    for(let i= 0; i < response.data.length; i++){
+      
+    }
+    document.getElementsByClassName("stp")[0].innerHTML = imgs;
+  })
+  .catch(error => {
+   
+  })
+}
+
+
+
+ 
+
+
+
+
+
+
+
+// const msg =
+
 
 defineProps<{ msg: string }>()
+
+
+
+  
+
+
 
 const count = ref(0)
 </script>
 
+
 <template>
   <h1>{{ msg }}</h1>
 
+
+
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <h1 class="stp">{{ imgs }}</h1>
+    <button type="button" @click="test()">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -28,6 +73,7 @@ const count = ref(0)
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
+  <select class="SelectionImages"></select>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
