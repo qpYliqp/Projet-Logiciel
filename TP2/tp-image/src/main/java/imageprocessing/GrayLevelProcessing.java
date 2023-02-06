1288da10463ba81dd5a75b397b0aa231df15bdfb
Yli <75490693+qpYliqp@users.noreply.github.com>
2023-02-06 14:19:49 +0100

    k

--- TP2/tp-image/src/main/java/imageprocessing/GrayLevelProcessing.java
+++ TP2/tp-image/src/main/java/imageprocessing/GrayLevelProcessing.java
@@ -2,6 +2,7 @@ package imageprocessing;
 
 import boofcv.alg.enhance.EnhanceImageOps;
 import boofcv.alg.misc.ImageStatistics;
+import boofcv.concurrency.BoofConcurrency;
 import boofcv.io.image.UtilImageIO;
 import boofcv.struct.image.GrayU8;
 
@@ -19,6 +20,45 @@ public class GrayLevelProcessing {
 				input.set(x, y, gl);
 			}
 		}
+	}
+
+	public static int[] histogram_threads(GrayU8 input, int thread) {
+		BoofConcurrency.setMaxThreads(thread);
+		// PARTIE 1 : CALCULER L'HISTOGRAMME
+		int[] histogram = new int[256];
+		for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				int pixel = input.get(x, y);
+				histogram[pixel]++;
+			}
+		}
+		// PARTIE 2 : CALCULER L'HISTOGRAMME CUMULÉ
+		int[] cumulative_histogram = new int[256];
+		cumulative_histogram[0] = histogram[0];
+		for (int i = 1; i < 256; i++) {
+			cumulative_histogram[i] = cumulative_histogram[i - 1] + histogram[i];
+		}
+
+		// PARTIE 3 : UTILISER L'HISTOGRAMME CUMULÉ COMME LUT
+		int min = cumulative_histogram[0];
+		int max = cumulative_histogram[255];
+		int[] lut = new int[256];
+		for (int i = 0; i < 256; i++) {
+			lut[i] = (int) (255.0 * (cumulative_histogram[i] - min) / (max - min));
+			if (lut[i] < 0) {
+				lut[i] = 0;
+			} else if (lut[i] > 255) {
+				lut[i] = 255;
+			}
+		}
+		BoofConcurrency.loopFor(0, input.height, y -> {
+			for (int x = 0; x < input.width; ++x) {
+				int pixel = input.get(x, y);
+				input.set(x, y, lut[pixel]);
+			}
+		});
+		System.out.print("Temps d'execution : " + System.nanoTime() + "\n");
+		return cumulative_histogram;
 	}
 
 	public static int[] histogram(GrayU8 input) {
@@ -55,13 +95,14 @@ public class GrayLevelProcessing {
 				input.set(x, y, lut[pixel]);
 			}
 		}
+		System.out.print("Temps d'execution : " + System.nanoTime() + "\n");
 		return cumulative_histogram;
 	}
 
-	public static void dynamique_lineaire_lookuptable(GrayU8 input, int min, int max) {
+	public static void dynamique_lineaire_lut(GrayU8 input, int min, int max) {
 		int[] lut = new int[256];
 		for (int i = 0; i < 256; i++) {
-			lut[i] = (int) (255.0 * (i - min) / (max - min));
+			lut[i] = (int) (255 * (i - min) / (max - min));
 			if (lut[i] < 0) {
 				lut[i] = 0;
 			} else if (lut[i] > 255) {
@@ -76,14 +117,43 @@ public class GrayLevelProcessing {
 		}
 		// Temps d'exection : 3033815639900 nano secondes pour min = 254 et max = 1
 		// On en déduit donc que cette fonction est plus rapide
-		System.out.print("Temps d'exection : " + System.nanoTime() + "\n");
+		System.out.print("Temps d'execution : " + System.nanoTime() + "\n");
+	}
+
+	public static void extend_gray(GrayU8 input) {
+		// Trouver la valeur max et min de l'image :
+		int max = 0, min = 10000000;
+		for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				int pixel = input.get(x, y);
+				if (pixel > max) {
+					max = pixel;
+				}
+				if (pixel < min) {
+					min = pixel;
+				}
+			}
+		}
+		// Appliquer la formule du cours
+		for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				int pixel = input.get(x, y);
+				pixel = (int) (255 * (pixel - min) / (max - min));
+				if (pixel < 0) {
+					pixel = 0;
+				} else if (pixel > 255) {
+					pixel = 255;
+				}
+				input.set(x, y, pixel);
+			}
+		}
 	}
 
 	public static void dynamique_lineaire(GrayU8 input, int min, int max) {
 		for (int y = 0; y < input.height; ++y) {
 			for (int x = 0; x < input.width; ++x) {
 				int pixel = input.get(x, y);
-				pixel = (int) (255.0 * (pixel - min) / (max - min));
+				pixel = (int) (255 * (pixel - min) / (max - min));
 				if (pixel < 0) {
 					pixel = 0;
 				} else if (pixel > 255) {
@@ -94,7 +164,7 @@ public class GrayLevelProcessing {
 		}
 		// Temps d'exection : 3082104035600 nano secondes pour min = 254 et max = 1
 		// On en déduit donc que cette fonction est plus lente
-		System.out.print("Temps d'exection : " + System.nanoTime());
+		System.out.print("Temps d'execution : " + System.nanoTime() + "\n");
 	}
 
 	public static void brightness(GrayU8 input, int delta) {
@@ -112,6 +182,17 @@ public class GrayLevelProcessing {
 		}
 	}
 
+	public static void boof_histogram(GrayU8 input, String output) {
+		int[] histogram = new int[256];
+		int[] transform = new int[256];
+
+		ImageStatistics.histogram(input, 0, histogram);
+		EnhanceImageOps.equalize(histogram, transform);
+		GrayU8 adjusted = input.createSameShape();
+		EnhanceImageOps.applyTransform(input, transform, adjusted);
+		UtilImageIO.saveImage(adjusted, output);
+	}
+
 	public static void main(String[] args) {
 
 		// load image
@@ -127,17 +208,16 @@ public class GrayLevelProcessing {
 		}
 
 		// histogram(input);
-		int[] histogram = new int[256];
-		int[] transform = new int[256];
-
-		ImageStatistics.histogram(input, 0, histogram);
-		EnhanceImageOps.equalize(histogram, transform);
-		GrayU8 adjusted = input.createSameShape();
-		EnhanceImageOps.applyTransform(input, transform, adjusted);
+		// extend_gray(input);
+		// dynamique_lineaire(input, 0, 255);
+		// dynamique_lineaire_lut(input, 0, 255);
+		histogram_threads(input, 5);
 
 		// save output image
 		final String outputPath = args[1];
-		UtilImageIO.saveImage(adjusted, outputPath);
+		/* Pour tester avec les outils de BoofCV : */ boof_histogram(input, outputPath);
+		/* Pour tester normalement : */ // UtilImageIO.saveImage(input, outputPath);
+		// UtilImageIO.saveImage(adjusted, outputPath);
 		System.out.println("Image saved in: " + outputPath);
 	}
 
