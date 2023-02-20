b09782492a4e7e13ab578de4234cafc6986f50e0
Yli <yliane.villot@hotmail.fr>
2023-02-20 12:26:22 +0100

    Convolution

--- TP2/tp-image/src/main/java/imageprocessing/Convolution.java
+++ TP2/tp-image/src/main/java/imageprocessing/Convolution.java
@@ -7,36 +7,166 @@ import boofcv.struct.border.BorderType;
 import boofcv.struct.convolve.Kernel1D_S32;
 import boofcv.struct.convolve.Kernel2D_S32;
 import boofcv.struct.image.GrayU8;
+//mvn exec:java '-Dexec.mainClass=imageprocessing.Convolution' '-Dexec.args=input_images/input.jpeg test.jpeg'
 
 public class Convolution {
 
+
+
+  /*______________________________________________________________________________________________
+   *______________________________________________________________________________________________
+   *__________________________________________MeanFilterSimple____________________________________
+   *______________________________________________________________________________________________
+   */
   public static void meanFilterSimple(GrayU8 input, GrayU8 output, int size) {
     for(int y = 0; y < input.height; y++) {
       for(int x = 0; x < input.width; x++) {
-        int sum = 0;
+        int somme = 0;
         for(int i = -size; i <= size; i++) {
           for(int j = -size; j <= size; j++) {
-            int xx = x + i;
-            int yy = y + j;
-            if(xx >= 0 && xx < input.width && yy >= 0 && yy < input.height) {
-              sum += input.get(xx, yy);
+            int u = x + i;
+            int v = y + j;
+            if(u >= 0 && u < input.width && v >= 0 && v < input.height) {
+              somme += input.get(u, v);
             }
           }
         }
-        output.set(x, y, sum / ((2 * size + 1) * (2 * size + 1)));
+        output.set(x, y, somme / ((2 * size + 1) * (2 * size + 1)));
       }
     }
   }
 
-  public static void meanFilterWithBorders(GrayU8 input, GrayU8 output, int size, BorderType borderType) {
-    
-  }
-  
-  public static void convolution(GrayU8 input, GrayU8 output, int[][] kernel) {
-    
+
+
+  /*______________________________________________________________________________________________
+   *______________________________________________________________________________________________
+   *__________________________________________MeanFilterWithBorders_______________________________
+   *______________________________________________________________________________________________
+   */
+  public static void meanFilterWithBorders(GrayU8 input, GrayU8 output, int size, BorderType borderType)
+  {
+    for(int y = 0; y < input.height; y++)
+    {
+      for(int x = 0; x < input.width; x++) 
+      {
+        int somme = 0;
+        for(int i = -size; i <= size; i++)
+        {
+          for(int j = -size; j <= size; j++)
+          {
+            int u = x + i;
+            int v = y + j;
+            if(u >= 0 && u < input.width && v >= 0 && v < input.height)
+            {
+              somme += input.get(u, v);
+            }
+            else
+            {
+              if(borderType == BorderType.EXTENDED){
+                if(u < 0)
+                {
+                  u = 0;
+                }              
+                else if(u >= input.width)
+                {
+                  //il faut mettre -1 car sinon on sort de l'image : "An exception occurred while executing the Java class. Requested pixel is out of bounds: 1920 0"
+                  u = input.width - 1;
+                }
+                if(v < 0)
+                {
+                  v = 0;
+                }
+                else if(v >= input.height)
+                {
+                  v = input.height - 1;
+                }
+                somme += input.get(u, v);
+              }
+              else if(borderType == BorderType.REFLECT)
+              {
+                somme += input.get(Math.abs(u), Math.abs(v));
+              }
+              else if(borderType == BorderType.SKIP)
+              {
+                somme += 0;
+              }
+              else if(borderType == BorderType.NORMALIZED){
+                
+                
+            }
+          }
+        }
+        output.set(x, y, somme / ((2 * size + 1) * (2 * size + 1)));
+      }
+     }
+    }
+
   }
 
-  public static void main(final String[] args) {
+  /*______________________________________________________________________________________________
+   *______________________________________________________________________________________________
+   *__________________________________________Convolution_______________________________
+   *______________________________________________________________________________________________
+   */
+  
+  // public static void convolution(GrayU8 input, GrayU8 output, int[][] kernel)
+  // {
+  //   for(int y = 0; y < input.height; y++) {
+  //     for(int x = 0; x < input.width; x++) {
+  //       int somme = 0;
+  //       for(int i = -1; i <= 1; i++) {
+  //         for(int j = -1; j <= 1; j++) {
+  //           int u = x + i;
+  //           int v = y + j;
+  //           if(u >= 0 && u < input.width && v >= 0 && v < input.height) {
+  //             somme += input.get(u, v) * kernel[i ][j ];
+  //           }
+  //         }
+  //       }
+  //       output.set(x, y, somme );
+  //     }
+  //   }
+  // }
+
+  public static void convolution(GrayU8 input, GrayU8 output, int[][] kernel)
+  {
+    int count = 0;
+    int size = kernel.length;
+    int n = (size-1)/2;
+    
+    for(int i = 0 ; i < kernel.length ; i++)
+    {
+      for(int j = 0 ; j < kernel[i].length ; j++)
+      {
+        count += kernel[i][j];
+      }
+      
+    }
+    for(int y = n; y < input.height - n; y++) {
+      for(int x = n; x < input.width - n; x++) {
+        int somme = 0;
+        for(int i = y-n; i <= y+n; i++)
+        {
+          for(int j = x-n; j <= x+n; j++)
+          {
+              somme += input.get(j, i) * kernel[i-y+n][j-x+n];
+              //System.out.println(kernel[i-y+n][j-x+n]);
+          }
+          //System.out.println(" input : " + input.get(x,y) + "\n output : " + somme/count);
+        }
+        output.set(x, y, somme/count);
+      }
+    }
+  }
+  
+  /*______________________________________________________________________________________________
+   *______________________________________________________________________________________________
+   *__________________________________________Gaussian_Filter_____________________________________
+   *______________________________________________________________________________________________
+   */
+
+
+  public static void main(final String[] args){
     // load image
     if (args.length < 2) {
       System.out.println("missing input or output image filename");
@@ -46,7 +176,52 @@ public class Convolution {
     GrayU8 input = UtilImageIO.loadImage(inputPath, GrayU8.class);
     GrayU8 output = input.createSameShape();
 
-    meanFilterSimple(input, output, 3);
+    int tab[][] = 
+    {{1,2,3,2,1},
+    {2,6,8,6,2},
+    {3,8,10,8,3},
+    {2,6,8,6,2},
+    {1,2,3,2,1}};
+
+    int matrice[][] = {
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+    };
+    // convolution(input, output, tab);
+    //meanFilterSimple(input, output, 5);
+
+    convolution(input, output, tab);
 
     // save output image
     final String outputPath = args[1];
--- TP2/tp-image/src/main/java/imageprocessing/GrayLevelProcessing.java
+++ TP2/tp-image/src/main/java/imageprocessing/GrayLevelProcessing.java
@@ -6,6 +6,9 @@ import boofcv.concurrency.BoofConcurrency;
 import boofcv.io.image.UtilImageIO;
 import boofcv.struct.image.GrayU8;
 
+//mvn exec:java '-Dexec.mainClass=imageprocessing.GrayLevelProcessing' '-Dexec.args=input_images/input.jpeg test.jpeg'
+
+
 public class GrayLevelProcessing {
 
 	public static void threshold(GrayU8 input, int t) {
@@ -21,6 +24,168 @@ public class GrayLevelProcessing {
 			}
 		}
 	}
+
+	/*__________________________________________________________________
+	* _________________________________________________________________
+	* ______________________brightness_________________________________
+	* _________________________________________________________________
+	*/
+	public static void brightness(GrayU8 input, int delta) {
+		for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				int pixel = input.get(x, y);
+				pixel += delta;
+				if (pixel < 0) {
+					pixel = 0;
+				} else if (pixel > 255) {
+					pixel = 255;
+				}
+				input.set(x, y, pixel);
+			}
+		}
+	}
+
+	/*__________________________________________________________________
+	 * _________________________________________________________________
+	 * ______________________extend_gray________________________________
+	 * _________________________________________________________________
+	 */
+
+	public static void extend_gray(GrayU8 input) {
+		// Trouver la valeur max et min de l'image :
+		int max = 0, min = 10000000;
+		for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				int pixel = input.get(x, y);
+				if (pixel > max) {
+					max = pixel;
+				}
+				if (pixel < min) {
+					min = pixel;
+				}
+			}
+		}
+		// Appliquer la formule du cours
+		for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				int pixel = input.get(x, y);
+				pixel = (int) (255 * (pixel - min) / (max - min));
+				if (pixel < 0) {
+					pixel = 0;
+				} else if (pixel > 255) {
+					pixel = 255;
+				}
+				input.set(x, y, pixel);
+			}
+		}
+	}
+
+
+	/*__________________________________________________________________
+	 * _________________________________________________________________
+	 * ______________________dynamique_lineaire_________________________
+	 * _________________________________________________________________
+	 */
+	public static void dynamique_lineaire(GrayU8 input, int min, int max) {
+		for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				int pixel = input.get(x, y);
+				pixel = (int) (255 * (pixel - min) / (max - min));
+				if (pixel < 0) {
+					pixel = 0;
+				} else if (pixel > 255) {
+					pixel = 255;
+				}
+				input.set(x, y, pixel);
+			}
+		}
+		// Temps d'exection : 3082104035600 nano secondes pour min = 254 et max = 1
+		// On en déduit donc que cette fonction est plus lente
+		System.out.print("Temps d'execution : " + System.nanoTime() + "\n");
+	}
+
+
+
+
+	/*__________________________________________________________________
+	 * _________________________________________________________________
+	 * ______________________dynamique_lineaire_lut_____________________
+	 * _________________________________________________________________
+	 */
+	public static void dynamique_lineaire_lut(GrayU8 input, int min, int max) {
+		int[] lut = new int[256];
+		for (int i = 0; i < 256; i++) {
+			lut[i] = (int) (255 * (i - min) / (max - min));
+			if (lut[i] < 0) {
+				lut[i] = 0;
+			} else if (lut[i] > 255) {
+				lut[i] = 255;
+			}
+		}
+		for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				int pixel = input.get(x, y);
+				input.set(x, y, lut[pixel]);
+			}
+		}
+		// Temps d'exection : 3033815639900 nano secondes pour min = 254 et max = 1
+		// On en déduit donc que cette fonction est plus rapide
+		System.out.print("Temps d'execution : " + System.nanoTime() + "\n");
+	}
+
+
+
+	/*__________________________________________________________________
+	 * _________________________________________________________________
+	 * ______________________histogram__________________________________
+	 * _________________________________________________________________
+	 */
+
+	 public static int[] histogram(GrayU8 input) {
+		// PARTIE 1 : CALCULER L'HISTOGRAMME
+		int[] histogram = new int[256];
+		for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				int pixel = input.get(x, y);
+				histogram[pixel]++;
+			}
+		}
+		// PARTIE 2 : CALCULER L'HISTOGRAMME CUMULÉ
+		int[] cumulative_histogram = new int[256];
+		cumulative_histogram[0] = histogram[0];
+		for (int i = 1; i < 256; i++) {
+			cumulative_histogram[i] = cumulative_histogram[i - 1] + histogram[i];
+		}
+
+		// PARTIE 3 : UTILISER L'HISTOGRAMME CUMULÉ COMME LUT
+		int min = cumulative_histogram[0];
+		int max = cumulative_histogram[255];
+		int[] lut = new int[256];
+		for (int i = 0; i < 256; i++) {
+			lut[i] = (int) (255.0 * (cumulative_histogram[i] - min) / (max - min));
+			if (lut[i] < 0) {
+				lut[i] = 0;
+			} else if (lut[i] > 255) {
+				lut[i] = 255;
+			}
+		}
+		for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				int pixel = input.get(x, y);
+				input.set(x, y, lut[pixel]);
+			}
+		}
+		System.out.print("Temps d'execution : " + System.nanoTime() + "\n");
+		return cumulative_histogram;
+	}
+
+
+
+	/*__________________________________________________________________
+	 * _________________________________________________________________
+	 * ______________________histogram_threads__________________________
+	 * _________________________________________________________________
+	 */
 
 	public static int[] histogram_threads(GrayU8 input, int thread) {
 		BoofConcurrency.setMaxThreads(thread);
@@ -61,137 +226,32 @@ public class GrayLevelProcessing {
 		return cumulative_histogram;
 	}
 
-	public static int[] histogram(GrayU8 input) {
-		// PARTIE 1 : CALCULER L'HISTOGRAMME
-		int[] histogram = new int[256];
-		for (int y = 0; y < input.height; ++y) {
-			for (int x = 0; x < input.width; ++x) {
-				int pixel = input.get(x, y);
-				histogram[pixel]++;
-			}
-		}
-		// PARTIE 2 : CALCULER L'HISTOGRAMME CUMULÉ
-		int[] cumulative_histogram = new int[256];
-		cumulative_histogram[0] = histogram[0];
-		for (int i = 1; i < 256; i++) {
-			cumulative_histogram[i] = cumulative_histogram[i - 1] + histogram[i];
+
+
+
+	/*__________________________________________________________________
+	* __________________________________________________________________
+	* ______________________boof_histogram______________________________
+	* __________________________________________________________________
+	*/
+
+	// Fonction qui permet de faire un histogramme cumulé avec BoofCV
+	// Elle permet aussi de générer directement l'image,
+	// Pour cette raison, il faut mettre en commentaire la ligne "UtilImageIO.saveImage(input, outputPath);" dans la fonction main
+		public static void boof_histogram(GrayU8 input, String output) {
+			int[] histogram = new int[256];
+			int[] transform = new int[256];
+
+			ImageStatistics.histogram(input, 0, histogram);
+			EnhanceImageOps.equalize(histogram, transform);
+			GrayU8 adjusted = input.createSameShape();
+			EnhanceImageOps.applyTransform(input, transform, adjusted);
+			UtilImageIO.saveImage(adjusted, output);
 		}
 
-		// PARTIE 3 : UTILISER L'HISTOGRAMME CUMULÉ COMME LUT
-		int min = cumulative_histogram[0];
-		int max = cumulative_histogram[255];
-		int[] lut = new int[256];
-		for (int i = 0; i < 256; i++) {
-			lut[i] = (int) (255.0 * (cumulative_histogram[i] - min) / (max - min));
-			if (lut[i] < 0) {
-				lut[i] = 0;
-			} else if (lut[i] > 255) {
-				lut[i] = 255;
-			}
-		}
-		for (int y = 0; y < input.height; ++y) {
-			for (int x = 0; x < input.width; ++x) {
-				int pixel = input.get(x, y);
-				input.set(x, y, lut[pixel]);
-			}
-		}
-		System.out.print("Temps d'execution : " + System.nanoTime() + "\n");
-		return cumulative_histogram;
-	}
 
-	public static void dynamique_lineaire_lut(GrayU8 input, int min, int max) {
-		int[] lut = new int[256];
-		for (int i = 0; i < 256; i++) {
-			lut[i] = (int) (255 * (i - min) / (max - min));
-			if (lut[i] < 0) {
-				lut[i] = 0;
-			} else if (lut[i] > 255) {
-				lut[i] = 255;
-			}
-		}
-		for (int y = 0; y < input.height; ++y) {
-			for (int x = 0; x < input.width; ++x) {
-				int pixel = input.get(x, y);
-				input.set(x, y, lut[pixel]);
-			}
-		}
-		// Temps d'exection : 3033815639900 nano secondes pour min = 254 et max = 1
-		// On en déduit donc que cette fonction est plus rapide
-		System.out.print("Temps d'execution : " + System.nanoTime() + "\n");
-	}
 
-	public static void extend_gray(GrayU8 input) {
-		// Trouver la valeur max et min de l'image :
-		int max = 0, min = 10000000;
-		for (int y = 0; y < input.height; ++y) {
-			for (int x = 0; x < input.width; ++x) {
-				int pixel = input.get(x, y);
-				if (pixel > max) {
-					max = pixel;
-				}
-				if (pixel < min) {
-					min = pixel;
-				}
-			}
-		}
-		// Appliquer la formule du cours
-		for (int y = 0; y < input.height; ++y) {
-			for (int x = 0; x < input.width; ++x) {
-				int pixel = input.get(x, y);
-				pixel = (int) (255 * (pixel - min) / (max - min));
-				if (pixel < 0) {
-					pixel = 0;
-				} else if (pixel > 255) {
-					pixel = 255;
-				}
-				input.set(x, y, pixel);
-			}
-		}
-	}
-
-	public static void dynamique_lineaire(GrayU8 input, int min, int max) {
-		for (int y = 0; y < input.height; ++y) {
-			for (int x = 0; x < input.width; ++x) {
-				int pixel = input.get(x, y);
-				pixel = (int) (255 * (pixel - min) / (max - min));
-				if (pixel < 0) {
-					pixel = 0;
-				} else if (pixel > 255) {
-					pixel = 255;
-				}
-				input.set(x, y, pixel);
-			}
-		}
-		// Temps d'exection : 3082104035600 nano secondes pour min = 254 et max = 1
-		// On en déduit donc que cette fonction est plus lente
-		System.out.print("Temps d'execution : " + System.nanoTime() + "\n");
-	}
-
-	public static void brightness(GrayU8 input, int delta) {
-		for (int y = 0; y < input.height; ++y) {
-			for (int x = 0; x < input.width; ++x) {
-				int pixel = input.get(x, y);
-				pixel += delta;
-				if (pixel < 0) {
-					pixel = 0;
-				} else if (pixel > 255) {
-					pixel = 255;
-				}
-				input.set(x, y, pixel);
-			}
-		}
-	}
-
-	public static void boof_histogram(GrayU8 input, String output) {
-		int[] histogram = new int[256];
-		int[] transform = new int[256];
-
-		ImageStatistics.histogram(input, 0, histogram);
-		EnhanceImageOps.equalize(histogram, transform);
-		GrayU8 adjusted = input.createSameShape();
-		EnhanceImageOps.applyTransform(input, transform, adjusted);
-		UtilImageIO.saveImage(adjusted, output);
-	}
+	/*________________________________________________________________________ */
 
 	public static void main(String[] args) {
 
@@ -207,6 +267,12 @@ public class GrayLevelProcessing {
 			System.exit(-1);
 		}
 
+		/*
+		 * _________________________________________________________________
+		 * _________________________________________________________________
+		 * _________________Liste des tests sans BoofCV_____________________
+		 * _________________________________________________________________
+		 */
 		// histogram(input);
 		// extend_gray(input);
 		// dynamique_lineaire(input, 0, 255);
@@ -215,8 +281,8 @@ public class GrayLevelProcessing {
 
 		// save output image
 		final String outputPath = args[1];
-		/* Pour tester avec les outils de BoofCV : */ boof_histogram(input, outputPath);
-		/* Pour tester normalement : */ // UtilImageIO.saveImage(input, outputPath);
+		/* POUR TESTER AVEC BOOFCV : */ //boof_histogram(input, outputPath);
+		/* Pour tester normalement : */ UtilImageIO.saveImage(input, outputPath);
 		// UtilImageIO.saveImage(adjusted, outputPath);
 		System.out.println("Image saved in: " + outputPath);
 	}
